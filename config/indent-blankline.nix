076bddb188cdf175b776e78b8acb68d8e0eb3d95
jonboh <jon.bosque.hernando@gmail.com>
2023-10-23 10:41:57 +0200

    fix ident-blankline

--- config/indent-blankline.nix
+++ config/indent-blankline.nix
@@ -1,17 +1,15 @@
 {
   config = {
-    plugins.indent-blankline = {
-      enable = true;
-      char = "▎";
-      spaceCharBlankline = "";
-      # TODO: fix context highlighting
-      showCurrentContext = true;
-      showCurrentContextStart = true;
-      showTrailingBlanklineIndent = false;
-    };
+    plugins.indent-blankline =
+      {
+        enable = true;
+      };
     options = {
       list = true;
       listchars = "space:⋅";
     };
+    extraConfigLua = ''
+      require("ibl").setup()
+    '';
   };
 }
