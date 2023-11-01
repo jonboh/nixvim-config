5877e179009747b33e3650143e8ed48c326a91ff
jonboh <jon.bosque.hernando@gmail.com>
2023-11-01 16:27:44 +0100

    add incremental selection

--- config/treesitter.nix
+++ config/treesitter.nix
@@ -1,5 +1,16 @@
 {
   plugins.treesitter = {
     enable = true;
+    incrementalSelection = {
+      enable = true;
+      keymaps = {
+        initSelection = "+";
+        nodeIncremental = "+";
+        nodeDecremental = "-";
+      };
+    };
+  };
+  plugins.treesitter-context = {
+    enable = true;
   };
 }
