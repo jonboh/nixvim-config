af9b809266b9e34b0254ffd12fe48ae3a0ed82d7
jonboh <jon.bosque.hernando@gmail.com>
2023-11-20 11:00:34 +0100

    remove centering on insert

--- config/autocmd.nix
+++ config/autocmd.nix
@@ -1,11 +1,5 @@
 {
   autoCmd = [
-    # Vertically center document when entering insert mode
-    {
-      event = "InsertEnter";
-      command = "norm zz";
-    }
-
     # Remove trailing whitespace on save
     {
       event = "BufWrite";
