10150c2f940c7b8662e8e97d7008605e3407ef83
jonboh <jon.bosque.hernando@gmail.com>
2024-02-14 10:17:53 +0100

    use latest image.nvim

--- overlays/image-nvim.nix
+++ overlays/image-nvim.nix
@@ -3,10 +3,10 @@ self: super:
   vimPlugins = super.vimPlugins // {
     image-nvim = super.vimPlugins.image-nvim.overrideAttrs (oldAttrs: {
       src = super.fetchFromGitHub {
-        owner = "jonboh";
+        owner = "3rd";
         repo = "image.nvim";
-        rev = "5c9d5d86cde747496abf452553a415196c38743f";
-        sha256 = "sha256-IBW6f+7T+UoeAIOwifLVRfhZE6bTmVktKy42vVEbrls=";
+        rev = "4c6cb5ad93ee93d8d7b7c84e1eb291cee99f0a0e";
+        sha256 = "sha256-mh3J3lW2Co2uA7YJzSGum0ZmpJBP0ZzBWUvJLAI9bHw=";
       };
     });
   };
