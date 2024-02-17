94a9a74d6e8e1cfbbd38e3a522d599afa872e599
jonboh <jon.bosque.hernando@gmail.com>
2024-02-17 19:36:28 +0100

    use VAULT_LOCATION for locating vault

--- config/krita.nix
+++ config/krita.nix
@@ -2,13 +2,14 @@
   extraConfigLua = ''
     local function open_file_krita()
         local filename = vim.fn.expand('<cfile>')
-        local modified_filename = string.gsub(filename, '.png', '.kra')
-        vim.cmd('!krita ' .. vim.fn.shellescape(modified_filename))
+        local full_project_filename = os.getenv("VAULT_LOCATION").."/" .. string.gsub(filename, '.png', '.kra')
+        vim.cmd('!krita ' .. vim.fn.shellescape(full_project_filename))
     end
     local function async_open_file_krita()
         local filename = vim.fn.expand('<cfile>')
-        local modified_filename = string.gsub(filename, '.png', '.kra')
-        vim.cmd('AsyncRun krita ' .. vim.fn.shellescape(modified_filename))
+        local full_project_filename = os.getenv("VAULT_LOCATION").."/" .. string.gsub(filename, '.png', '.kra')
+        print(full_project_filename)
+        vim.cmd('AsyncRun krita ' .. vim.fn.shellescape(full_project_filename))
     end
     vim.api.nvim_create_user_command('OpenInKritaAsync', async_open_file_krita, {})
     vim.api.nvim_create_user_command('OpenInKrita', open_file_krita, {})
