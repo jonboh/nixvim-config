61c4d34bfaaf55b5f5e69a51cfc557fabfef4cf0
jonboh <jon.bosque.hernando@gmail.com>
2024-03-26 11:07:30 +0100

    add image toggle

--- config/image.nix
+++ config/image.nix
@@ -1,1 +1,42 @@
-{ plugins.image.enable = true; }
+{
+  plugins.image = {
+    enable = true;
+
+    # windowOverlapClearEnabled = true;
+    # windowOverlapClearFtIgnore = ["cmp_menu" "cmp_docs" ""];
+  };
+
+  extraConfigLua = ''
+    vim.api.nvim_create_user_command("ImageClear", function(args)
+      local img = require("image")
+      local images = img.get_images()
+      for i, image in pairs(images) do
+        img.clear(image.id)
+      end
+    end,
+    { desc="Clear Images" }
+    )
+
+    vim.api.nvim_create_user_command("ImageRender", function(args)
+      local img = require("image")
+      local images = img.get_images()
+      for i, image in pairs(images) do
+        img.render(image.id)
+      end
+    end,
+    { desc="Render Images" })
+  '';
+
+  keymaps = [
+    {
+      mode = "n";
+      key = "<leader>i";
+      action = "<cmd>ImageClear<cr>";
+    }
+    {
+      mode = "n";
+      key = "<leader>I";
+      action = "<cmd>ImageRender<cr>";
+    }
+  ];
+}
