eb0ada4393afc8ffabb3313154602b3254c69da1
jonboh <jon.bosque.hernando@gmail.com>
2024-04-04 14:42:27 +0200

    add documentation

--- config/devdocs.nix
+++ config/devdocs.nix
@@ -3,7 +3,22 @@
 
   extraConfigLua = ''
     require("nvim-devdocs").setup({
-      ensure_installed = {"rust", "nix", "python-3.11"}, -- get automatically installed
+      ensure_installed = { -- get automatically installed
+        "rust",
+        "nix",
+        "lua-5.1",
+        "latex",
+        "python-3.11",
+        "numpy-1.23",
+        "pandas-1",
+        "matplotlib-3.7",
+        "pytorch-2",
+        "scikit_learn",
+        "bash",
+        "jq",
+        "postgresql-16",
+        "influxdata",
+        },
       })
   '';
 
