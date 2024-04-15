e37d1cf016d1bbe16d2c5925527d0d86a37dc2e5
jonboh <jon.bosque.hernando@gmail.com>
2024-04-15 10:58:21 +0200

    add context indicator

--- config/indent-blankline.nix
+++ config/indent-blankline.nix
@@ -1,12 +1,26 @@
-{
+{pkgs, ...}: {
   config = {
-    plugins.indent-blankline = {enable = true;};
     options = {
       list = true;
       listchars = "space:⋅";
     };
+    extraPlugins = [pkgs.vimPlugins.indent-blankline-nvim];
     extraConfigLua = ''
-      require("ibl").setup()
+      local highlight = {
+          "RainbowViolet",
+      }
+      local hooks = require "ibl.hooks"
+      -- create the highlight groups in the highlight setup hook, so they are reset
+      -- every time the colorscheme changes
+      hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+          vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+      end)
+
+      vim.g.rainbow_delimiters = { highlight = highlight }
+      require("ibl").setup { scope = { highlight = highlight } }
+
+      hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
+
     '';
   };
 }
