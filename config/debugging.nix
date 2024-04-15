82b6496354431e1a5d4defb9754312a43d5cc49e
jonboh <jon.bosque.hernando@gmail.com>
2024-04-15 13:11:45 +0200

    helper similar to P but to file

--- config/debugging.nix
+++ config/debugging.nix
@@ -4,5 +4,11 @@
         print(vim.inspect(v))
         return v
     end
+
+    W = function(v, filename)
+      local file = io.open(filename or 'stdout_nvim_lua', 'a') -- Open the file in append mode
+      file:write(vim.inspect(v))
+      file:close()
+    end
   '';
 }
