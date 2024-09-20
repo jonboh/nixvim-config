0e3d2a2c7392e29ba008308e54499c7d6d09ac0d
jonboh <jon.bosque.hernando@gmail.com>
2024-09-20 23:16:51 +0200

    build latex in build dir

--- config/vimtex.nix
+++ config/vimtex.nix
@@ -4,4 +4,20 @@
     # full is needed for Neorg render-latex
     texlivePackage = pkgs.texlive.combined.scheme-full;
   };
+  extraConfigVim = ''
+    let g:vimtex_compiler_latexmk = {
+        \ 'aux_dir' : 'build',
+        \ 'out_dir' : 'build',
+        \ 'callback' : 1,
+        \ 'continuous' : 1,
+        \ 'executable' : 'latexmk',
+        \ 'hooks' : [],
+        \ 'options' : [
+        \   '-verbose',
+        \   '-file-line-error',
+        \   '-synctex=1',
+        \   '-interaction=nonstopmode',
+        \ ],
+        \}
+  '';
 }
