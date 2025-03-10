753dc8ec55365d574b59c0ae08fb55d45e5a22ae
jonboh <jon.bosque.hernando@gmail.com>
2025-03-10 19:19:04 +0100

    add luasnip shortcuts for jumping between nodes

--- config/snippets.nix
+++ config/snippets.nix
@@ -2,9 +2,50 @@
   plugins.luasnip = {enable = true;};
   extraPlugins = [pkgs.vimPlugins.friendly-snippets];
   extraConfigLua = ''
-        require("luasnip.loaders.from_vscode").lazy_load() -- following instructions from friendly-snippets.nvim
-      local ls = require "luasnip"
+    require("luasnip.loaders.from_vscode").lazy_load() -- following instructions from friendly-snippets.nvim
+    local ls = require "luasnip"
     local types = require("luasnip.util.types")
+
+    ls.config.set_config {
+        -- This tells LuaSnip to keep around the last snippet. You can jump back into it if you move outside the selection
+        history = true,
+
+        -- This is for dynamic snippets, it updates the snippet as you type. I'm not sure I actually use this
+        updateevents = "TextChanged,TextChangedI",
+
+        -- Autosnippets
+        -- enable_autosnippets = true;
+
+        ext_opts = {
+          [types.choiceNode] = {
+              active = { virt_text = {{"<-", "Error"}}}
+            }
+          }
+      }
+    vim.keymap.set({"i", "s"}, "<C-h>", function()
+      if ls.expand_or_jumpable() then
+        ls.expand_or_jump()
+      end
+    end, {silent=true})
+
+    vim.keymap.set({"i", "s"}, "<C-,>", function()
+      if ls.jumpable(-1) then
+        ls.jump(-1)
+      end
+    end, {silent=true})
+    vim.keymap.set({"i"}, "<CS-h>", function()
+      if ls.choice_active() then
+        ls.change_choice(1)
+      end
+    end, {silent=true})
+    vim.keymap.set({"i"}, "<CS-,>", function()
+      if ls.choice_active() then
+        ls.change_choice(-1)
+      end
+    end, {silent=true})
+
+
+    -- vim.keymap.set("n", "<leader><leader>s", "<cmd>source <mutabel snippets_location><cr>")
 
     local s = ls.snippet
     local sn = ls.snippet_node
@@ -15,6 +56,15 @@
     local d = ls.dynamic_node
     local r = ls.restore_node
 
+    -- ls.add_snippets("nix", {
+    --   s("test_choice",{
+    --   c(1, {
+    --       t "CHOICE1",
+    --       t "CHOICE2",
+    --       t "CHOICE3",
+    --     })
+    --   })
+    -- })
     ls.add_snippets("rust", {
       s('allowfreedom', t '#![allow(clippy::disallowed_names, unused_variables, dead_code)]'),
 
