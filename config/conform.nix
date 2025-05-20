1222377a6e2fbd1d606c82b9eadc850dcd2ff31f
jonboh <jon.bosque.hernando@gmail.com>
2025-05-20 10:03:13 +0200

    fix slow formatters functionality

--- config/conform.nix
+++ config/conform.nix
@@ -69,43 +69,56 @@
         sql = ["sql-formatter"];
         mysql = ["sql-formatter-mysql"];
         julia = ["runic"];
-        "*" = ["trim_whitespace"];
+        "*" = [
+          "trim_whitespace"
+          "trim_newlines"
+        ];
       };
-      format_on_save = ''
-        function(bufnr)
-          -- Disable with a global or buffer-local variable
-          if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
-            return
-          end
-
-          -- Handle slow formatters
-          if _conform_slow_format_filetypes[vim.bo[bufnr].filetype] then
-            return
-          end
-          local function on_format(err)
-            if err and err:match("timeout$") then
-              _conform_slow_format_filetypes[vim.bo[bufnr].filetype] = true
+      format_on_save =
+        # Lua
+        ''
+          function(bufnr)
+            if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+              return
             end
-          end
 
-          return { timeout_ms = 200, lsp_fallback = false }
-        end
-      '';
+            if slow_format_filetypes[vim.bo[bufnr].filetype] then
+              return
+            end
 
-      settings.format_after_save = ''
-        function(bufnr)
-          if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
-            return
+            local function on_format(err)
+              if err and err:match("timeout$") then
+                slow_format_filetypes[vim.bo[bufnr].filetype] = true
+              end
+            end
+
+            return { timeout_ms = 200, lsp_fallback = true }, on_format
+           end
+        '';
+
+      format_after_save =
+        # Lua
+        ''
+          function(bufnr)
+            if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+              return
+            end
+
+            if not slow_format_filetypes[vim.bo[bufnr].filetype] then
+              return
+            end
+
+            return { lsp_fallback = true }
           end
-          if not _conform_slow_format_filetypes[vim.bo[bufnr].filetype] then
-            return
-          end
-          return { lsp_fallback = false }
-        end
-      '';
+        '';
+
+      log_level = "warn";
+      notify_on_error = false;
+      notify_no_formatters = false;
     };
   };
   extraConfigLua = ''
+    slow_format_filetypes = {}
     -- Set Format commands
     vim.api.nvim_create_user_command("Format", function(args)
       local range = nil
