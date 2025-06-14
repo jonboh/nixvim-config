86ddcfbc78a55dc6a831bbb8d275ae42524a6e4d
jonboh <jon.bosque.hernando@gmail.com>
2025-06-14 12:13:16 +0200

    add arrow mappings for rocq

--- config/coq.nix
+++ config/coq.nix
@@ -1,3 +1,14 @@
 {pkgs, ...}: {
   extraPlugins = [pkgs.vimPlugins.Coqtail];
+
+  extraConfigVim = ''
+    function CoqtailHookDefineMappings()
+      imap <buffer> <S-Down> <Plug>RocqNext
+      imap <buffer> <S-Left> <Plug>RocqToLine
+      imap <buffer> <S-Up> <Plug>RocqUndo
+      nmap <buffer> <S-Down> <Plug>RocqNext
+      nmap <buffer> <S-Left> <Plug>RocqToLine
+      nmap <buffer> <S-Up> <Plug>RocqUndo
+    endfunction
+  '';
 }
