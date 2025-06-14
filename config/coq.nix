9599e8f6f4f1fcf19045b7506abf9620d71f5a8a
jonboh <jon.bosque.hernando@gmail.com>
2025-06-14 18:09:27 +0200

    move cursor when running

--- config/coq.nix
+++ config/coq.nix
@@ -3,12 +3,12 @@
 
   extraConfigVim = ''
     function CoqtailHookDefineMappings()
-      imap <buffer> <S-Down> <Plug>RocqNext
+      imap <buffer> <S-Down> <Plug>RocqNext <Plug>RocqJumpToEnd
       imap <buffer> <S-Left> <Plug>RocqToLine
-      imap <buffer> <S-Up> <Plug>RocqUndo
-      nmap <buffer> <S-Down> <Plug>RocqNext
+      imap <buffer> <S-Up> <Plug>RocqUndo <Plug>RocqJumpToEnd
+      nmap <buffer> <S-Down> <Plug>RocqNext <Plug>RocqJumpToEnd
       nmap <buffer> <S-Left> <Plug>RocqToLine
-      nmap <buffer> <S-Up> <Plug>RocqUndo
+      nmap <buffer> <S-Up> <Plug>RocqUndo <Plug>RocqJumpToEnd
     endfunction
   '';
 }
