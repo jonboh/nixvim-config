ac54472cf62556a4a58998e2fa86d28e5ee994e8
jonboh <jon.bosque.hernando@gmail.com>
2025-07-29 11:25:19 +0200

    fix async krita not working ok (just 1 image is opened) if krita was not already opened

--- config/krita.nix
+++ config/krita.nix
@@ -1,17 +1,11 @@
 {
   extraConfigLua = ''
-    local function open_file_krita()
-        local filename = vim.fn.expand('<cfile>')
-        local full_project_filename = os.getenv("VAULT_LOCATION").."/" .. string.gsub(filename, '.png', '.kra')
-        vim.cmd('!krita ' .. vim.fn.shellescape(full_project_filename))
-    end
     local function async_open_file_krita()
         local filename = vim.fn.expand('<cfile>')
         local full_project_filename = os.getenv("VAULT_LOCATION").."/" .. string.gsub(filename, '.png', '.kra')
-        print(full_project_filename)
-        vim.cmd('AsyncRun krita ' .. vim.fn.shellescape(full_project_filename))
+        -- print(full_project_filename)
+        vim.system({'krita', full_project_filename})
     end
-    vim.api.nvim_create_user_command('OpenInKritaAsync', async_open_file_krita, {})
-    vim.api.nvim_create_user_command('OpenInKrita', open_file_krita, {})
+    vim.api.nvim_create_user_command('OpenInKrita', async_open_file_krita, {})
   '';
 }
