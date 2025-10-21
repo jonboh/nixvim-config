85436a75433f5c3d96231bf90688d0e02d61f3d0
jonboh <jon.bosque.hernando@gmail.com>
2025-10-21 19:14:57 +0200

    handle vault blocked

--- config/azure-prs.nix
+++ config/azure-prs.nix
@@ -3,12 +3,27 @@
   extraConfigLua = ''
     vim.g.adopure = {}
 
-    local nio = require("nio")
-    nio.run(function()
-        local secret_job = nio.process.run({ cmd = "pass", args = { "show", "azure-pat"} })
-        vim.g.adopure = { pat_token = secret_job.stdout.read():sub(1, -2) }
-    end)
+    local function reload_azure_pat()
+      local nio = require("nio")
+      nio.run(function()
+        local check_vault_job = nio.process.run({ cmd = "is_vault_unlocked", args= { "jon@jonboh.dev" }})
+        if check_vault_job.stdout.read() == "" then
+            vim.notify("pass vault is locked. Could not retrieve Azure PAT.", vim.log.levels.WARN)
+        else
+            local secret_job = nio.process.run({ cmd = "pass", args = { "show", "azure-pat"} })
+            vim.g.adopure = { pat_token = secret_job.stdout.read():sub(1, -2) }
+        end
+      end)
+    end
 
+    -- Initial load
+    local ok, err = pcall(reload_azure_pat)
+    if not ok then
+      print("AdoPure initialization error: " .. tostring(err))
+    end
+
+    -- Create command to reload PAT
+    vim.api.nvim_create_user_command('AdoPurePatReload', reload_azure_pat, {})
   '';
 
   plugins.diffview.enable = true;
