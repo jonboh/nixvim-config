3bbc9297032e55658adfff3db94d93314898038a
jonboh <jon.bosque.hernando@gmail.com>
2026-02-25 21:54:42 +0100

    add snacks.image

--- config/snacks.nix
+++ config/snacks.nix
@@ -1,4 +1,4 @@
-{
+{pkgs, ...}: {
   plugins.snacks = {
     enable = true;
     settings = {
@@ -7,8 +7,32 @@
       picker.enable = true;
       notifier.enable = true;
       terminal.enable = true;
+      image = {
+        enable = true;
+        resolve.__raw = ''
+          function(path, src)
+            local api = require "obsidian.api"
+            if api.path_is_note(path) then
+              return api.resolve_attachment_path(src)
+            end
+          end'';
+
+        doc = {
+          inline = false;
+          float = true;
+        };
+
+        math = {
+          enabled = true;
+          latex = {
+            font_size = "large";
+            packages = ["amsmath" "amssymb" "amsfonts" "amscd" "mathtools"];
+          };
+        };
+      };
     };
   };
+  extraPackages = [pkgs.ghostscript pkgs.imagemagick pkgs.mermaid-cli]; # these are for snacks.image
 
   userCommands = {
     Notifications = {
