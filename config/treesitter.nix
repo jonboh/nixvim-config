0d3bee8ef6cde6d5a39ddfa901f07f8d2a73e2f4
jonboh <jon.bosque.hernando@gmail.com>
2026-03-01 22:06:27 +0100

    incremental selection no longer available on treesitter rewrite

--- config/treesitter.nix
+++ config/treesitter.nix
@@ -3,14 +3,6 @@
     treesitter = {
       enable = true;
       settings = {
-        incremental_selection = {
-          enable = true;
-          keymaps = {
-            init_selection = "+";
-            node_incremental = "+";
-            node_decremental = "-";
-          };
-        };
         highlight = {
           enable = true;
           disable = ["csv"]; # csv highlight in nvim is better than treesitter
